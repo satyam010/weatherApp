088e45d3c229acea8968270f84a83f0d0c071a03
satyam010 <43791247+satyam010@users.noreply.github.com>
2018-10-03 16:18:16 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ function getWeather(){
     if(city != ''){
         
         $.ajax({
-           url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=c10bb3bd22f90d636baa008b1529ee25",
+           url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=c10bb3bd22f90d636baa008b1529ee25",
             type: "GET",
             dataType: "jsonp",
             success: function(data){
